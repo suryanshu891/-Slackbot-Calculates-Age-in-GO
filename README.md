d3366ced6e0df1af80055c188c82d7745606aa89
suryanshu891 <137926436+suryanshu891@users.noreply.github.com>
2023-08-02 21:55:40 +0530

    tested code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# -Slackbot-Calculates-Age-in-GO
-GO-Slackbot-Calculates-Age is a project that enables a Slackbot to calculate the age of a person based on their birthdate. Developed using Go, this Slackbot can receive input from users in the form of birthdate and provides the age as output. 
+# Slackbot-Calculates-Age
+Demo bot to show you how to perform some basic ops.
+This code accompanies my youtube video where I've taught how to create a slackbot from complete scratch
+Link for the video - https://youtu.be/HnPm69i60xE
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/shomali11/slacker"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Evenets")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5514038956774-5517937601301-0f7IBR0pUAkNhO0uojEDpW8K")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A05G0DZ2Q3S-5506238968887-15919e73d94135bb910375a7e0aec30a7725f29af6bd79b22997d490a66dd1c4")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
+		Description: "yob calculator",
+		Example:     "my yob is 2020",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				println("error")
+			}
+			age := 2021 - yob
+			r := fmt.Sprintf("age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+		
+	}
+}
+
